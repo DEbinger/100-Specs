430498bf58793cbb7755efd5b5d4eba3474585c1
David <davidebinger888@gmail.com>
2016-12-06 23:57:37 -1000

    step 59

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1086,7 +1086,11 @@ Scientist.prototype.addDiscovery = function(newton){
  * Meal class
  * @param {Array} foods All the foods in the meal
  */
-
+ class Meal{
+  constructor(foods){
+    this.foods = foods;
+  }
+ }
 
 /**
  * Create a new instance of the Classes you defined above, below!
